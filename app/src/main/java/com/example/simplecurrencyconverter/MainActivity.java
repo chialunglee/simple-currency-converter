8df23e6143107998adf7fcc2fd3c558effd3fdf8
chialunglee <jimmylee7890@gmail.com>
2020-09-08 23:35:23 +0800

    新增簡單的計算行為

--- app/src/main/java/com/example/simplecurrencyconverter/MainActivity.java
+++ app/src/main/java/com/example/simplecurrencyconverter/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.simplecurrencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void convertCurrency(View view) {
+        EditText numberInput = (EditText) findViewById(R.id.numberInput);
+        String originalInput = numberInput.getText().toString();
+        if (originalInput.isEmpty()) {
+            Toast.makeText(this, "請輸入數字！", Toast.LENGTH_LONG).show();
+        }
+        else {
+            double convertedInput = Double.parseDouble(originalInput);
+            double result = convertedInput * 1.5;
+            String outputText = String.format("%.2f", result);
+            Toast.makeText(this, outputText, Toast.LENGTH_LONG).show();
+        }
     }
 }
